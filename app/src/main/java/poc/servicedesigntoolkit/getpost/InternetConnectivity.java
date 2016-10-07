c5ba3ce50a06751e5233c270b98bf7e3ad755cee
Gunjan Pathak <e0015263@u.nus.edu>
2016-10-07 12:46:07 +0800

    List View of the touchpoints

---
+++ app/src/main/java/poc/servicedesigntoolkit/getpost/InternetConnectivity.java
@@ -0,0 +1,13 @@
+package poc.servicedesigntoolkit.getpost;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by Gunjan Pathak on 06-Oct-16.
+ */
+
+public class InternetConnectivity {
+
+}
---
+++ app/src/main/java/poc/servicedesigntoolkit/getpost/TouchpointDetails.java
@@ -0,0 +1,8 @@
+package poc.servicedesigntoolkit.getpost;
+
+/**
+ * Created by Gunjan Pathak on 06-Oct-16.
+ */
+
+public class TouchpointDetails {
+}
